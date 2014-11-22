5899bf7f31b8596b7dbfb1c3c5eb18f2f1aba4a2
Gareth Redfern <gareth.redfern@gmail.com>
2014-11-22 21:52:26 +0000

    Updated nav styles and markup.

--- js/main.js
+++ js/main.js
@@ -1,39 +1,57 @@
 // RESPONSIVE NAV
-// ==============================================================
-function responsiveNav(windowSize) {
+// =============================================================================
+var mainNav = function(menuList,menuBtn) {
 
-  var nav = $('#nav');
+  var s,
+  GrNav = {
 
-  $('nav').on('click', '#nav__button', function() {
-    var $this = $(this);
-    nav.slideToggle('fast');
-    $this.toggleClass('open');
-    $this.find('.arrow-down').toggle();
-    $this.find('.arrow-up').toggle();
-  });
+    settings: {
+      menuList: $(menuList),
+      menuBtn: $(menuBtn),
+      windowSize: 960,
+    },
 
-  $(window).resize(function() {
-    var width = $(window).width();
-    if (width < windowSize) {
-      nav.attr( "style", "" );
+    init: function() {
+      s = this.settings;
+      this.navClick();
+      this.responsiveNav();
+      this.hideMobileBar();
+    },
+
+    // hide the iphone url bar
+    hideMobileBar: function() {
+      /mobi/i.test(navigator.userAgent) && !location.hash && setTimeout(function () {
+        if (!pageYOffset) window.scrollTo(0, 1);
+      }, 1000);
+    },
+
+    // slide the menu on click
+    navClick: function() {
+      s.menuBtn.on('click', function() {
+        s.menuList.slideToggle(300);
+      });
+    },
+
+    // mobile resize function
+    responsiveNav: function() {
+      $(window).resize(function() {
+        var width = $(window).width();
+        if (width < s.windowSize) {
+          s.menuList.attr("style", "");
+        }
+      });
     }
-  });
 
-  // Listen for resize changes
-  window.addEventListener("resize", function() {
-    var navButton = $('#nav__button');
-    nav.attr( "style", "" );
-    navButton.removeClass('open');
-    navButton.find('.arrow-down').show();
-    navButton.find('.arrow-up').hide();
-  }, false);
+  };
 
-}
+  GrNav.init();
 
-responsiveNav(960);
+};
+
+mainNav('#js-nav--primary__list', '#menu');
 
 // FITVIDS
-// ==============================================================
+// =============================================================================
 
 $(document).ready(function(){
   $(".article__video").fitVids();
